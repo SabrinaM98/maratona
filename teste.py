f8db2c82bb08f8e344f34b1cd832543551c2910c
SabrinaM98 <61629793+SabrinaM98@users.noreply.github.com>
2020-03-08 11:50:16 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -175,6 +175,39 @@ danmei2 = [['HC', 'XL', 'SQX'], ['LBH','SQQ','LQG'], ['CWN','MR']]
         for j in i:
             print(j)
 ///////////////////////////////////////////////////////////////////////////////////////
+repetir = 's'
+fatura = []
+total = 0
+
+while repetir == 's':
+    produto = input('digite o nome do prod')
+    preco = float(input('digite o preco dele'))
+    fatura.append([produto, preco])
+    total += preco
+    repetir = input('quer continuar? (s/n)').lower()
+for i in fatura:
+    print(i[0], '-', i[1])
+print('total da fatura e: ', total)
+
+#nesse codigo, da ruim se alguem digitar 10,00 como preco. o input nao consegue converter pra float
+repetir = 's'
+fatura = []
+total = 0
+valid_preco = False
+while repetir == 's':
+    produto = input('digite o nome do prod')
+    preco = float(input('digite o preco dele'))
+    while valid_preco == False:
+      Try:
+        preco = float(preco)
+        valid_preco
+    fatura.append([produto, preco])
+    total += preco
+    repetir = input('quer continuar? (s/n)').lower()
+for i in fatura:
+    print(i[0], '-', i[1])
+print('total da fatura e: ', total)
+//////////////////////////////////////////////////////////////////////////////
 
         
    
